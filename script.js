89f04c49a80d684308c34f4d68bc0ba043b5707a
Caio Lopes <caiogustavolopes20@gmail.com>
2025-09-09 12:55:53 -0300

    Demo

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+ })
+
+ menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+ })
+ menu.addEventListener('click', ()=>{
+    overlay.classList.remove('abrir-menu')
+ })
+
